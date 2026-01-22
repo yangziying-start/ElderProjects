5ff54e9231e6ec9633deabdb0ef78290b58e2393
yangziying-start <1924209451@qq.com>
2026-01-22 20:47:37 +0800

    Initialize Spring Boot application for Elderly service

---
+++ backend/src/main/java/com/elderly/ElderlyApplication.java
@@ -0,0 +1,11 @@
+package com.elderly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElderlyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElderlyApplication.class, args);
+    }
+}
