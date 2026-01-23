81d75ff661517372d783fa51a9bf91b64e65c4f9
yangziying-start <1924209451@qq.com>
2026-01-23 16:00:17 +0800

    Delete backend/src/main directory

--- backend/src/main/java/com/elderly/mapper/EmergencyCallMapper.java
+++
@@ -1,9 +0,0 @@
-package com.elderly.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.elderly.entity.EmergencyCall;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface EmergencyCallMapper extends BaseMapper<EmergencyCall> {
-}
--- backend/src/main/java/com/elderly/mapper/ServiceItemMapper.java
+++
@@ -1,9 +0,0 @@
-package com.elderly.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.elderly.entity.ServiceItem;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ServiceItemMapper extends BaseMapper<ServiceItem> {
-}
