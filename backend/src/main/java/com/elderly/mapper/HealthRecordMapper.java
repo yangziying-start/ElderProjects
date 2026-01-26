2bdaeef643ed0c99958bdbf238be92dd3338b0bb
yangziying-start <1924209451@qq.com>
2026-01-26 19:44:31 +0800

    Delete backend/src/main/java/com/elderly directory

--- backend/src/main/java/com/elderly/mapper/HealthRecordMapper.java
+++
@@ -1,9 +0,0 @@
-package com.elderly.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.elderly.entity.HealthRecord;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface HealthRecordMapper extends BaseMapper<HealthRecord> {
-}
